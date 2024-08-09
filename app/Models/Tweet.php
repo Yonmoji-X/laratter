d995acb3644f69da75e07be219dbe8dda4381534
Yonmoji-X <yametu55@gmail.com>
2024-08-09 16:52:50 +0900

    tweet機能、詳細画面追加

---
+++ app/Models/Tweet.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['tweet'];
+
+    public function user()
+    {
+        // tweet多数：user1
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function tweets()
+    {
+         // user1：tweet多数
+        return $this->hasMany(Tweet::class);
+    }
 }
